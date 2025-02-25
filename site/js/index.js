5964cde482e02b7ed5d6c913d4d5d90da8f4fd40
fernandohenrique0202 <fernando.costa105@etec.sp.gov.br>
2025-02-24 21:58:43 -0300

    Add files via upload

---
+++ site/js/index.js
@@ -0,0 +1,26 @@
+function logar(event){
+        //impede o envio normal do formulario//
+    event.preventDefault();
+
+    var usuario = document.getElementById('usuario').value;
+    var senha = document.getElementById('senha').value;
+
+    if(usuario == 'admin' && senha == 'admin'){
+        Swal.fire({
+            title:'login realizado',
+            text:'Bem-vindo, ' + usuario,
+            icon:'success',
+            confirmButtomText:'OK'
+
+        }).then(() => {
+            setTimeout(() => {
+              location.href="./html/home.html";
+            }, 100);
+        });
+
+
+    }else{
+        alert('ERRO usuário ou senha incorretos');
+    }
+
+}
