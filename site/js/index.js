c95c05be27ee59f09cad06ce7b8c63e5392c63e9
fernandohenrique0202 <fernando.costa105@etec.sp.gov.br>
2025-03-10 21:24:15 -0300

    Add files via upload

--- site/js/index.js
+++ site/js/index.js
@@ -1,26 +1,34 @@
-function logar(event){
-        //impede o envio normal do formulario//
+function logar(event) {
     event.preventDefault();
 
     var usuario = document.getElementById('usuario').value;
     var senha = document.getElementById('senha').value;
 
-    if(usuario == 'admin' && senha == 'admin'){
+    if (usuario == 'admin' && senha == 'admin') {
         Swal.fire({
-            title:'login realizado',
-            text:'Bem-vindo, ' + usuario,
-            icon:'success',
-            confirmButtomText:'OK'
-
+            title: 'Login realizado!',
+            text: 'Bem-vindo, ' + usuario + '!',
+            icon: 'success',
+            confirmButtonText: 'OK'
         }).then(() => {
             setTimeout(() => {
-              location.href="./html/home.html";
-            }, 100);
+                location.href = "./html/home.html";
+            }, 100); 
         });
-
-
-    }else{
-        alert('ERRO usuário ou senha incorretos');
+    } else {
+        Swal.fire({
+            title: 'Erro!',
+            text: 'Usuário ou senha incorretos.',
+            icon: 'error',
+            confirmButtonText: 'Tentar novamente'
+        });
     }
-
 }
+
+    
+
+
+
+
+
+
